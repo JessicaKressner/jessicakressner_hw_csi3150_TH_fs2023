e86de325e9a160ea8da91e93b529af9da2e915b2
Jessica <jessicakressner@oakland.edu>
2023-12-07 13:56:30 -0500

    Initial message

---
+++ App.js
@@ -0,0 +1,120 @@
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+
+// any additional imports
+import Task from "./components/Task";
+
+export default function App() {
+  // input area
+  const [task, setTask] = useState();
+
+  // to store all tasks
+  const [taskItems, setTaskItems] = useState([]);
+
+  // event listener logic
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems({ ...taskItems, task });
+    setTask(null);
+  };
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* List all the to do items */}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>My To Do Items</Text>
+
+        {/* container or wrapper for the tasks to be rendered */}
+        <View style={styles.items}>
+          {/* <Task text={"this is task 1"} /> */}
+          {taskItems.map((item, index) => {
+            return (
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Task text={item} />
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      </View>
+      {/* User input for todo tasks */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Add your to do item here"}
+          value={task}
+          onChangeText={(text) => setTask(task)}
+        />
+
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.sectionTitle}>My To Do Items</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  addText: {},
+});
